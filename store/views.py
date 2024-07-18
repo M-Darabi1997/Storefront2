33ebd4246d18aef4d79c5287b7085a89d04f5245
Mohammad <darabi.mohammad1997@gmail.com>
2024-07-18 22:50:05 +0330

    implemented searching and sorting for products in the browsable api

--- store/views.py
+++ store/views.py
@@ -1,11 +1,9 @@
 
 from django.db.models.aggregates import Count
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from rest_framework.filters import SearchFilter, OrderingFilter
 from store.filters import ProductFilter
 from .models import OrderItem, Product, Collection, Review
 from .serializers import ProductSerializer,CollectionSerializer, ReviewSerializer
@@ -17,8 +15,10 @@ class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter ]
     filterset_class = ProductFilter
+    search_fields = ['title', 'description']
+    ordering_fields = ['unit_price', 'last_update']
 
     def get_serializer_context(self):
         return {'request': self.request}
