1d66d8ef3ca5b507b9643a3f455cf4fc62743421
Mohammad <darabi.mohammad1997@gmail.com>
2024-07-18 22:26:01 +0330

    Added the nested route for products/reviews

--- store/views.py
+++ store/views.py
@@ -33,5 +33,11 @@ class CollectionViewSet(ModelViewSet):
 # Create your views here.
 
 class ReviewViewSet(ModelViewSet):
-    queryset = Review.objects.all()
+
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk']) 
+    
     serializer_class = ReviewSerializer
+
+    def get_serializer_context(self):
+        return {'product_id': self.kwargs['product_pk']}
