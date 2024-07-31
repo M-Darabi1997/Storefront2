8c01c2bedffa093e0cbf32c6d702b6bd4faae12f
Mohammad <darabi.mohammad1997@gmail.com>
2024-07-31 17:01:00 +0330

    built the signals.py file for the store app and implemented a handler to create a customer when a user is created

--- store/views.py
+++ store/views.py
@@ -88,7 +88,7 @@ class CustomerViewSet(ModelViewSet):
     @action(detail=False, methods=['GET', 'PUT'], permission_classes=[IsAuthenticated])
     def me(self, request):
         
-        (customer, created) = Customer.objects.get_or_create(user_id=request.user.id)
+        customer = Customer.objects.get(user_id=request.user.id)
         if request.method == 'GET':
         
             serializer = CustomerSerializer(customer)
@@ -101,7 +101,7 @@ class CustomerViewSet(ModelViewSet):
         
 class OrderViewSet(ModelViewSet):
     
-    http_method_names = ['get', 'patch', 'delete', 'head', 'options']
+    http_method_names = ['get', 'post', 'patch', 'delete', 'head', 'options']
 
     def get_permissions(self):
         if self.request.method in ['PATCH', 'DELETE']:
@@ -127,5 +127,5 @@ class OrderViewSet(ModelViewSet):
     def get_queryset(self):
         if self.request.user.is_staff:
             return Order.objects.all()
-        (customer_id, created) = Customer.objects.only('id').get_or_create(user_id=self.request.user.id)
+        customer_id = Customer.objects.only('id').get(user_id=self.request.user.id)
         return Order.objects.filter(customer_id=customer_id)
